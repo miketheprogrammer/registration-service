7cfe5628c43d82b47669bff25f3675010a5a6c11
Michael Hernandez <michael.hernandez1988@gmail.com>
2015-12-13 23:24:31 -0500

    finalizing

--- Dockerfile
+++ Dockerfile
@@ -4,11 +4,10 @@ RUN apk add --update openssh
 RUN apk add --update git
 RUN rm -rf /var/cache/apk/*
 RUN apk add --update bash
-#RUN npm install -g pm2
+RUN npm install -g pm2
 WORKDIR /opt/registration-service
 ADD ./package.json ./package.json
 ADD ./index.js ./index.js
 ADD ./run.sh ./run.sh
 RUN npm install
-#CMD pm2 start index.js -i 2 --name=maestro-cluster && pm2 logs
-CMD node index.js
+CMD pm2 start index.js -i 2 --name=registration-service && pm2 logs
--- index.js
+++ index.js
@@ -15,9 +15,14 @@ setInterval(function () {
       active.forEach(function (id) {
         if (res.indexOf(id) === -1) {
       	  console.log('Reaping', id);
-          var reg = registrations[id];
-          // client.unregister(reg.role, reg.version, reg.port)
-	        delete registrations[id]
+          var container = registrations[id];
+          consul.agent.service.deregister({name: container.Labels['com.application.name'], id: container.Id, port: container.Ports[0].PublicPort}, function(err) {
+            if (err) {
+              console.warn(err, err.stack);
+            }
+            // client.unregister(reg.role, reg.version, reg.port)
+	         delete registrations[id]
+         })
         }
       })
     })
@@ -26,45 +31,56 @@ setInterval(function () {
 }, tick)
 
 function register(container, cb) {
-  if (container.Labels['com.application.name'] === undefined) {
-    console.log('skipping', container.Id, container.Labels);
-    return cb(null, container.Id);
-  }
-  if (registrations[container.Id] === 'healthcheck') {
-    console.log(container.Id, 'awating healthcheck');
-    return cb(null, container.Id);
-  }
-  if (registrations[container.Id] !== undefined) {
-    console.log(container.Id, 'already registered')
-    return cb(null, container.Id);
-  }
+  try {
+    if (container.Labels['com.application.name'] === undefined) {
+      console.log('skipping', container.Id, container.Labels);
+      return cb(null, container.Id);
+    }
+    if (registrations[container.Id] === 'healthcheck') {
+      console.log(container.Id, 'awating healthcheck');
+      return cb(null, container.Id);
+    }
+    if (registrations[container.Id] !== undefined) {
+      console.log(container.Id, 'already registered')
+      return cb(null, container.Id);
+    }
 
-  // use a seperator of ':::'
-  var serviceName = container.Labels['com.application.name'];
-  console.log('Registering', serviceName)
+    // use a seperator of ':::'
+    var serviceName = container.Labels['com.application.name'];
+    console.log('Registering', serviceName)
+  } catch (err) {
+    console.warn(err, err.stack)
+    return cb(null, '');
+  }
   consul.agent.service.register({name: serviceName, id: container.Id, port: container.Ports[0].PublicPort}, function(err) {
-    if (err) throw cb(err);
+    if (err) {
+      console.warn(err, err.stack)
+      throw cb(null, '');
+    }
+    registrations[container.Id] = container;
+    console.log('Registered', container.Id)
+    return cb(null, container.Id);
   });
   
 }
 //healthcheck register
-function _register (container, meta, maxRetries, cb) {
-  registrations[container.Id] = 'healthcheck';
-  if (maxRetries === 0) {
-    delete registrations[container.Id]
-    return cb(null, container.Id)
-  }
+// function _register (container, meta, maxRetries, cb) {
+//   registrations[container.Id] = 'healthcheck';
+//   if (maxRetries === 0) {
+//     delete registrations[container.Id]
+//     return cb(null, container.Id)
+//   }
 
-  request('http://localhost:' + meta.port, function (err, response, body) {
-    if (err) {
-      return setTimeout(function () {
-        _register(container, meta, maxRetries - 1, cb);
-      }, 1000);
-    } else { 
-      console.log(container.Id, 'passed healthcheck with body', body);
-      client.register(meta.role, meta.version, meta.port, meta);
-      registrations[container.Id] = meta;
-      return cb(null, container.Id)
-    }
-  });
-}
+//   request('http://localhost:' + meta.port, function (err, response, body) {
+//     if (err) {
+//       return setTimeout(function () {
+//         _register(container, meta, maxRetries - 1, cb);
+//       }, 1000);
+//     } else { 
+//       console.log(container.Id, 'passed healthcheck with body', body);
+//       client.register(meta.role, meta.version, meta.port, meta);
+//       registrations[container.Id] = meta;
+//       return cb(null, container.Id)
+//     }
+//   });
+// }
